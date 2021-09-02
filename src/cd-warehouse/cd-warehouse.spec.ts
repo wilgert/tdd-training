841c1364df35fdd7b1c9cf4226cce25d84705394
Wilgert Velinga <wilgert@wilgert.nl>
2021-09-02 20:11:51 +0200

    cd warehouse: handle adding 1 cd that is new

--- src/cd-warehouse/cd-warehouse.spec.ts
+++ src/cd-warehouse/cd-warehouse.spec.ts
@@ -1,15 +1,32 @@
+interface Stock {
+  [key: string]: number;
+}
+
 class WareHouse {
+  constructor(private stock: Stock) {}
+
   getStock() {
-    return { Elephant: 1 };
+    return this.stock;
+  }
+
+  addCds(stockItems: { copies: number; title: string }[]) {
+    stockItems.forEach((stockItem) => {
+      if (!this.stock[stockItem.title]) {
+        this.stock[stockItem.title] = 0;
+      }
+
+      this.stock[stockItem.title] += stockItem.copies;
+    });
   }
 }
 
 describe('cd warehouse', () => {
-  describe('Record label', () => {
-    describe(`Send batch of cd's`, () => {
+  describe('record label', () => {
+    describe(`send batch of cd's`, () => {
       describe('send 1 copy of a cd that is new', () => {
         it('adds the cd', () => {
-          const warehouse = new WareHouse();
+          const warehouse = new WareHouse({});
+          warehouse.addCds([{ title: 'Elephant', copies: 1 }]);
           expect(warehouse.getStock()).toEqual({ Elephant: 1 });
         });
       });
