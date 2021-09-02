3ae6a889791944c6be65e84d50fd2353dac3aa01
Wilgert Velinga <wilgert@wilgert.nl>
2021-09-02 20:13:11 +0200

    refactor: move warehouse and stock to separate files

--- src/cd-warehouse/cd-warehouse.spec.ts
+++ src/cd-warehouse/cd-warehouse.spec.ts
@@ -1,31 +1,11 @@
-interface Stock {
-  [key: string]: number;
-}
-
-class WareHouse {
-  constructor(private stock: Stock) {}
-
-  getStock() {
-    return this.stock;
-  }
-
-  addCds(stockItems: { copies: number; title: string }[]) {
-    stockItems.forEach((stockItem) => {
-      if (!this.stock[stockItem.title]) {
-        this.stock[stockItem.title] = 0;
-      }
-
-      this.stock[stockItem.title] += stockItem.copies;
-    });
-  }
-}
+import { Warehouse } from './warehouse';
 
 describe('cd warehouse', () => {
   describe('record label', () => {
     describe(`send batch of cd's`, () => {
       describe('send 1 copy of a cd that is new', () => {
         it('adds the cd', () => {
-          const warehouse = new WareHouse({});
+          const warehouse = new Warehouse({});
           warehouse.addCds([{ title: 'Elephant', copies: 1 }]);
           expect(warehouse.getStock()).toEqual({ Elephant: 1 });
         });
---
+++ src/cd-warehouse/warehouse.ts
@@ -0,0 +1,19 @@
+import { Stock } from './stock';
+
+export class Warehouse {
+  constructor(private stock: Stock) {}
+
+  getStock() {
+    return this.stock;
+  }
+
+  addCds(stockItems: { copies: number; title: string }[]) {
+    stockItems.forEach((stockItem) => {
+      if (!this.stock[stockItem.title]) {
+        this.stock[stockItem.title] = 0;
+      }
+
+      this.stock[stockItem.title] += stockItem.copies;
+    });
+  }
+}
