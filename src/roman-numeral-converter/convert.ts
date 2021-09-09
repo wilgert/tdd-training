f2f8122a75315200c2de715f1c11036a17e1d77b
Wilgert Velinga <wilgert@wilgert.nl>
2021-09-09 17:42:57 +0200

    implement 5, 6, 7

--- src/roman-numeral-converter/convert.ts
+++ src/roman-numeral-converter/convert.ts
@@ -1,7 +1,9 @@
 export function convert(input: number): string {
   if (input < 4) {
     return 'I'.repeat(input);
-  } else {
+  } else if (input === 4) {
     return 'IV';
+  } else {
+    return 'V' + 'I'.repeat(input - 5);
   }
 }
--- src/roman-numeral-converter/roman-numeral-converter.spec.ts
+++ src/roman-numeral-converter/roman-numeral-converter.spec.ts
@@ -1,4 +1,18 @@
 import { convert } from './convert';
+/*
+X converts 1 -> I
+x converts 2 -> II
+x converts 3 -> III
+x converts 4 -> IV
+x converts 5 -> V
+converts 6 -> VI
+converts 7 -> VII
+converts 8 -> IIX
+converts 9 -> IX
+converts 10 -> X
+...
+converts 3999
+*/
 
 describe('Roman numeral converter', () => {
   it.each`
@@ -7,6 +21,9 @@ describe('Roman numeral converter', () => {
     ${2}  | ${'II'}
     ${3}  | ${'III'}
     ${4}  | ${'IV'}
+    ${5}  | ${'V'}
+    ${6}  | ${'VI'}
+    ${7}  | ${'VII'}
   `('converts $input to "$expected"', ({ input, expected }) => {
     expect(convert(input)).toEqual(expected);
   });
