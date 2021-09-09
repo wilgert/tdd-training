c0711b6e2eca90558d09bd213766177d05de6c62
Wilgert Velinga <wilgert@wilgert.nl>
2021-09-09 19:24:13 +0200

    refactor: use characters list and output string

--- src/roman-numeral-converter/convert.ts
+++ src/roman-numeral-converter/convert.ts
@@ -1,9 +1,18 @@
 export function convert(input: number): string {
+  const characters = 'IVXLCDM';
+  let output = '';
+
   if (input < 4) {
-    return 'I'.repeat(input);
-  } else if (input === 4) {
-    return 'IV';
-  } else {
-    return 'V' + 'I'.repeat(input - 5);
+    output = characters[0].repeat(input);
   }
+
+  if (input === 4) {
+    output = characters[0] + characters[1];
+  }
+
+  if (input >= 5) {
+    output = characters[1] + characters[0].repeat(input - 5);
+  }
+
+  return output;
 }
