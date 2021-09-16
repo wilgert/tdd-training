94965bc5d02f603c5720a88bf7f0f04303406e86
Wilgert Velinga <wilgert@wilgert.nl>
2021-09-16 19:50:16 +0200

    implement buying item without going out of stock

--- src/guitar-shack/guitar-shack.spec.ts
+++ src/guitar-shack/guitar-shack.spec.ts
@@ -1,3 +1,22 @@
+import { GuitarShack } from './guitar.shack';
+
 describe('Guitar Shack', () => {
-  it('', () => {});
+  it('does not send alert when stock after sale above restock level', () => {
+    const alertShopManager = jest.fn();
+
+    let stockLevels = { get: jest.fn().mockReturnValue(4) };
+    let restockLevels = { get: jest.fn().mockReturnValue(2) };
+
+    let guitarShack = new GuitarShack(
+      alertShopManager,
+      stockLevels,
+      restockLevels
+    );
+    let productId = 42;
+    let quantity = 1;
+
+    guitarShack.buy(productId, quantity);
+
+    expect(alertShopManager).not.toHaveBeenCalled();
+  });
 });
